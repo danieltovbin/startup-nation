777a0cd0e34373f459c6f27850ef2814e5a5c2a1
danieltovbin <tovbindaniel@gmail.com>
2024-01-29 16:24:39 +0200

    styling

--- client/src/components/descriptionEdit/DescriptionProject.tsx
+++ client/src/components/descriptionEdit/DescriptionProject.tsx
@@ -2,24 +2,26 @@ import './scss/description.scss'
 
 const DescriptionProject = () => {
   return (
-    <div className="description" style={{padding: "0 calc((100% - 818px) / 2) !important",}}>
+    <div className="description">
+      <div className='description-container'>
         <p>איך מתארים את הפרויקט בצורה הטובה ביותר? המטרה היא לעניין אנשים, למשוך אותם פנימה לתוך הפרויקט, אנחנו רוצים ליצור רושם ראשוני מסקרן ומעניין</p>
-        <form style={{display:'flex' ,flexDirection: "column",padding: "0 calc((100% - 818px) / 2) !important"}}>
+        <form style={{display:'flex' ,flexDirection: "column", maxWidth: "580px"}}>
             <label htmlFor="">שם הפרויקט</label>
             <input type="text" />
             <label htmlFor="">תיאור קצר של הפרויקט</label>
-            <input type="text" />
-            <label htmlFor="">קטגוריות נוספות לפרויקט(סה''כ ניתן להגדיר עד 3 קטגוריות)</label>
-            <input type="text" />
+            <input style={{height: "68px",padding: "11px"}} type="text" />
+            <p className='title'>קטגוריות נוספות לפרויקט<span> (סה''כ ניתן להגדיר עד 3 קטגוריות)</span></p>
+            <input style={{height: "40px"}} type="text" />
             <label htmlFor="">תגיות הפרויקט(לא חובה)</label>
             <input type="text" />
             <label htmlFor="">תמונת הפרויקט</label>
-            <input type="text" />
+            <input type="file" />
             <label htmlFor="">סרטון הפרויקט(לא חובה)</label>
             <input type="text" />
             <label htmlFor="">סכום הגיוס</label>
             <input type="text" />
         </form>
+        </div>
     </div>
   )
 }
