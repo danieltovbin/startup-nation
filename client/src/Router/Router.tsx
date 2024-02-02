9151bef022d0169b3e184ad04e29e9b675bc34a2
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2024-02-02 14:40:10 +0200

    ruth

--- client/src/Router/Router.tsx
+++ client/src/Router/Router.tsx
@@ -2,12 +2,14 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage/LoginPage";
 import RegisterPage from "../Pages/RegisterPage/RegisterPage";
 import HomePage from "../Pages/Home/HomePage";
+import CreateProject from "../Pages/CreateProjectPage/CreateProjectPage";
 
 const Router = () => {
   const router = createBrowserRouter([
     { path: "/login", element: <LoginPage/> },
     { path: "/register", element: <RegisterPage /> },
     { path: "/homePage", element: <HomePage /> },
+    { path: "/createProject", element: <CreateProject /> },
     { path: "/", element: <LoginPage /> },
   ])
 
